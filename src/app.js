3cc6a65a25530c1cc9c04d5d93c127fcde7ba683
unknown <fois2010@gmail.com>
2022-12-29 16:48:04 -0300

    mongodb mongoose listando os produtos

--- src/app.js
+++ src/app.js
@@ -3,10 +3,20 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const app = express();
 const router = express.Router();
+
+// Conecta com o banco de Dados
+mongoose.connect('mongodb+srv://fois2010:Siexpre$$@cluster0.9e3ejn1.mongodb.net/?retryWrites=true&w=majority');
+
+
+
+// Carrega os Models
+const Product = require('./models/product');
+
 
 // Carrega as Rotas
 const indexRoute = require('./routes/index-route');
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 
 // POST
+router.get('/', controller.get)
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
