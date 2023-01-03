15a8e829a0e1a277b8c7d1ccc401c75b5f5c89a5
unknown <fois2010@gmail.com>
2023-01-03 14:09:32 -0300

    Configuraçao sendgrid

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'fb862e15-ab85-4559-981d-ec160b53777c';
-global.EMAIL_TMPL = '<strong>{0}</strong>'
+global.EMAIL_TMPL = 'olá, <strong>{0}</strong>, seja bem vindo ao Node Store';
 
 module.exports = {
     connectionString: 'mongodb+srv://fois2010:Siexpre$$@cluster0.9e3ejn1.mongodb.net/?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
+    sendgridKey: 'TSG.9_Uv4ZckQIibmLbNwLKp4g.miCP714Sq0W0VnaK0BXi6xjbvjOkLjsTnqIJhQ3f3EA',
     containerConnectionString: 'TBD'
 }
